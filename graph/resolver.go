72375c17c877bfdfc1b85044f7092647f8f345c0
tomato3713 <tomato3713@outlook.com>
2023-09-07 22:38:41 +0900

    import bun

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,26 @@
 package graph
 
+import (
+	"context"
+	"log/slog"
+
+	"github.com/uptrace/bun"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	ctx    *context.Context
+	logger *slog.Logger
+	db     *bun.DB
+}
+
+func NewResolver(ctx *context.Context, logger *slog.Logger, db *bun.DB) *Resolver {
+	return &Resolver{
+		ctx:    ctx,
+		logger: logger,
+		db:     db,
+	}
+}
