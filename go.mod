445a82f6eac2d5466804e854725485fe088567ba
tomato3713 <tomato3713@hatena.ne.jp>
2023-09-07 22:38:41 +0900

    import bun

--- go.mod
+++ go.mod
@@ -1,11 +1,15 @@
 module github.com/tomato3713/nulledge
 
-go 1.21.0
+go 1.20
 
 require (
 	github.com/99designs/gqlgen v0.17.36
 	github.com/cespare/reflex v0.3.1
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/uptrace/bun v1.1.14
+	github.com/uptrace/bun/dialect/mysqldialect v1.1.14
 	github.com/vektah/gqlparser/v2 v2.5.8
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 )
 
 require (
@@ -21,14 +25,13 @@ require (
 	github.com/ogier/pflag v0.0.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
-	github.com/uptrace/bun v1.1.14 // indirect
 	github.com/urfave/cli/v2 v2.25.5 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server.go
+++ server.go
@@ -1,28 +1,53 @@
 package main
 
 import (
-	"log"
+	"context"
+	"database/sql"
+	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/tomato3713/nulledge/graph"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mysqldialect"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	ctx := context.Background()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	sqldb, err := sql.Open("mysql", getDbConnStr())
+	if err != nil {
+		panic(err)
+	}
+
+	db := bun.NewDB(sqldb, mysqldialect.New())
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	resolver := graph.NewResolver(&ctx, logger, db)
+
+	srv := handler.NewDefaultServer(
+		graph.NewExecutableSchema(graph.Config{Resolvers: resolver}),
+	)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	logger.Info("connect to http://localhost:" + port + "/ for GraphQL playground")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		logger.Warn("Exit", "err", err)
+	}
+}
+
+func getDbConnStr() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_ADDR"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_TABLENAME"))
 }
