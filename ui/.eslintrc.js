da2fcf6e13876d8ff9718001e6a854cd51ad2e40
tomato3713 <tomato3713@hatena.ne.jp>
2023-11-01 00:56:09 +0900

    ignore packages extension

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -11,5 +11,15 @@ module.exports = {
   },
   rules: {
     "import/prefer-default-export": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
   },
 };
