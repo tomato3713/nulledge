29529b16fab4edb3c1d9858bda90203dde8b5351
tomato3713 <tomato3713@outlook.com>
2023-11-01 00:56:33 +0900

    apolloprovider

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,7 +1,28 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  const link = createHttpLink({
+    uri: "http://localhost:8080/query",
+    credentials: "include",
+  });
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link,
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
